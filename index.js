02fdaa6b75fd1fc44475eba44153b21a7663f5a3
hariskhan101 <111196879+hariskhan101@users.noreply.github.com>
2022-09-15 19:58:08 +0500

    Add initial Game website files

---
+++ index.js
@@ -0,0 +1,26 @@
+
+
+var randomNumber1 = Math.random() * 6;
+randomNumber1 = Math.floor(randomNumber1) + 1;
+var randomDiceImage = "dice" +randomNumber1+ ".png";
+var randomImageSource = "images/" +randomDiceImage;
+
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.random() * 6;
+randomNumber2 = Math.floor(randomNumber2) + 1;
+var randomImageSource =  "images/dice" +randomNumber2+ ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource);
+
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins";
+
+} else if (randomNumber1 === randomNumber2 ) {
+  document.querySelector("h1").innerHTML = "Draw";
+}
+else {
+  document.querySelector("h1").innerHTML = "Player 2 Wins";
+}
